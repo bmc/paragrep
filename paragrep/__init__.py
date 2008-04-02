e870a1ac061567e23725b6c8560a72c42fd4cd3a
Brian Clapper <bmc@clapper.org>
2008-04-02 01:03:07 +0000

    fixed docs

--- paragrep/__init__.py
+++ paragrep/__init__.py
@@ -1,66 +1,147 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
-# Uses epydoc markup.
-'''
+"""
 paragrep - Paragraph Grep utility
 
 Usage
 =====
 
-paragrep [-aiotv] [-p eop_regexp] [-e regexp] ... [-f exp_file] ...
-         [file ] ... 
-                       -OR-
-paragrep [-itv] [-p eop_regexp] regexp [file] ...
+B{paragrep} [-aiotv] [-p I{eop_regexp}] [-e I{regexp}] ... [-f I{exp_file}] ... [I{file} ] ... 
+
+B{paragrep} [-itv] [-p I{eop_regexp}] I{regexp} [I{file}] ...
 
 
+Options
+-------
 
-Options:
---------
+Summary of options::
 
--h, --help            Show this message and exit.
+    -h, --help            Show this message and exit.
 
--a, --and             Logically AND all regular expressions.
+    -a, --and             Logically AND all regular expressions.
 
--o, --or              Logically OR all regular expressions.
+    -o, --or              Logically OR all regular expressions.
 
--i, --caseblind       Match without regard to case.
+    -i, --caseblind       Match without regard to case.
 
--v, --negate          Negate the sense of the match.
+    -v, --negate          Negate the sense of the match.
 
--e REGEXPS            Specify a regular expression to find.This option may
---regexp=REGEXPS      be specified multiple times.
---expr=REGEXPS
+    -e regexp             Specify a regular expression to find.This option may
+    --regexp=regexp       be specified multiple times.
+    --expr=regexp
                       
--p EOP_REGEXP         Specify an alternate regular expression to match end-
---eop=EOP_REGEXP      of-paragraph. Default: ^\s*$
+    -p eop_regexp         Specify an alternate regular expression to match end-
+    --eop=eop_regexp      of-paragraph. Default: ^\s*$
                       
-                      
--f EXPR_FILE          Specify a file full of regular expressions, one per
---file=EXPR_FILE      line.
+    -f expr_file          Specify a file full of regular expressions, one per
+    --file=expr_file      line.
 
 
-Description:
-------------
+Description
+===========
 
-paragrep is a paragraph grep utility. It searches for a series of regular
-expressions in a text file (or several text files) and prints out the
-paragraphs containing those expressions. Normally a paragraph is displayed
-if it contains any of the expressions; this behavior can be modified by
-using the -a option.
+B{paragrep} is a paragraph grep utility. It searches for a series of
+regular expressions in a text file (or several text files) and prints out
+the paragraphs containing those expressions. Normally B{paragrep} displays
+a paragraph if it contains any of the expressions; this behavior can be
+modified by using the C{-a} option.
 
 By default, a paragraph is defined as a block of text delimited by an empty
-or blank line; this behavior can be altered with the -p option.
+or blank line; this behavior can be altered with the C{-p} option.
 
-If no files are specified on the command line, paragrep searches standard
-input.
+If no files are specified on the command line, B{paragrep} searches
+standard input.
 
-This is the third implementation of paragrep. The first implementation, in
-1989, was in C. The second implementation, in 2003, was in perl. This is the
-latest and greatest.
+This is the third implementation of B{paragrep}. The first implementation,
+in 1989, was in C. The second implementation, in 2003, was in perl. This is
+the latest and greatest.
 
-Copyright and License:
-----------------------
+Options in Detail
+-----------------
+
+C{-a}
+~~~~~
+
+The I{and} option: Only display a paragraph if it contains I{all} the
+regular expressions specified. The default is to display a paragraph if
+it contains I{any} of the regular expressions. See the C{-o} option, below.
+
+C{-e} I{expression}
+~~~~~~~~~~~~~~~~~~~
+
+Adds a regular expression to the set of expressions to use when matching
+paragraphs. More than one C{-e} argument may be specified. If there's only
+one expression, the C{-e} may be omitted for brevity. (Think I{sed}.)
+
+C{-f} I{expfile}
+~~~~~~~~~~~~~~~~
+
+Specifies a file containing regular expressions, one expression per line.
+Each expression in the file is added to the set of expression against which
+paragraphs are to be matched.   More than one C{-f} argument is permitted.
+Also, C{-f} and C{-e} may be specified together.
+
+C{-i}
+~~~~~
+
+Considers upper- and lower-case letters to be identical when making comparisons.
+
+C{-o}
+~~~~~
+
+The I{or} option: Display a paragraph if it contains I{any} the
+regular expressions specified. Since this option is the default, it is
+rarely specified on the command line. It exists primarily to negate the
+effect of a previous C{-a} option. (e.g., If you've defined an alias for
+B{paragrep} that specifies the C{-a} option, C{-o} would be necessary to
+force the I{or} behavior.)
+
+C{-p} I{eop_expression}
+~~~~~~~~~~~~~~~~~~~~~~~
+
+Specifies a regular expression to be used match paragraph delimiters.  Any
+line that matches this regular expression is assumed to delimit paragraphs
+without actually being part of a paragraph (i.e., lines matching this
+expression are never printed).  If this option is not specified, it
+defaults to::
+
+    ^[ \\t]*$
+
+which matches blank or empty lines. (C{\\t} represents the horizontal tab
+character. If you need to specify a horizontal tab, you'll need to type the
+actual character; B{paragrep} doesn't recognize C-style metacharacters.)
+
+C{-v}
+~~~~~
+
+Displays all lines that do not match specified expressions.  The negation
+logic works on DeMorgan's Laws.  Normally, if C{-a} is specified,
+B{paragrep} uses the following logic to match the paragraph::
+
+    match = contains(expr1) AND contains(expr2) ... 
+
+Specifying C{-v} along with C{-a} changes this logic to::
+
+    match = lacks(expr1) OR lacks(expr2) ...
+
+Likewise, without C{-a} or C{-v} (i.e., using C{-o}, which is the default),
+the matching logic is::
+
+    match = contains(expr1) OR contains(expr2) ... 
+
+Negating that logic with C{-v} causes paragrep to match paragraphs with::
+
+    match = lacks(expr1) AND lacks(expr2) ... 
+
+See Also
+========
+
+ - The Unix I{grep}(1) command
+ - The Python U{C{re} module<http://docs.python.org/lib/module-re.html>}
+
+Copyright and License
+=====================
 
 Copyright © 1989-2008 Brian M. Clapper
 
@@ -69,20 +150,20 @@ This is free software, released under the following BSD-like license:
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice,
+ - Redistributions of source code must retain the above copyright notice,
    this list of conditions and the following disclaimer.
 
-2. The end-user documentation included with the redistribution, if any,
+ - The end-user documentation included with the redistribution, if any,
    must include the following acknowlegement:
 
-      This product includes software developed by Brian M. Clapper
-      (bmc@clapper.org, http://www.clapper.org/bmc/). That software is
-      copyright © 2008 Brian M. Clapper.
+   This product includes software developed by Brian M. Clapper
+   (bmc@clapper.org, http://www.clapper.org/bmc/). That software is
+   copyright \169 2008 Brian M. Clapper.
 
-    Alternately, this acknowlegement may appear in the software itself, if
-    and wherever such third-party acknowlegements normally appear.
+   Alternately, this acknowlegement may appear in the software itself, if
+   and wherever such third-party acknowlegements normally appear.
 
-THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+THIS SOFTWARE IS PROVIDED B{AS IS} AND ANY EXPRESSED OR IMPLIED
 WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
 EVENT SHALL BRIAN M. CLAPPER BE LIABLE FOR ANY DIRECT, INDIRECT,
@@ -92,7 +173,7 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-'''
+"""
 
 # $Id$
 
@@ -101,7 +182,7 @@ __version__   = '3.0'
 __author__    = 'Brian Clapper'
 __email__     = 'bmc@clapper.org'
 __url__       = 'http://www.clapper.org/software/python/paragrep/'
-__copyright__ = '© 1989-2008 Brian M. Clapper'
+__copyright__ = '1989-2008 Brian M. Clapper'
 __license__   = 'BSD-style license'
 
 # Package stuff
