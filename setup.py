3d8949a8843b8bbec8955711be299bcb8cee2bf8
Brian Clapper <bmc@clapper.org>
2008-05-17 00:36:36 +0000

    grizzled version change

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup (name             = 'paragrep',
        author           = info['__author__'],
        author_email     = info['__email__'],
        entry_points     = {'console_scripts' : 'paragrep=paragrep:main'},
-       install_requires = ['grizzled>=0.1'],
+       install_requires = ['grizzled>=0.4'],
        data_files       = [('man', ['man/paragrep.1'])],
        classifiers = [
         'Intended Audience :: Developers',
