aea8224022ec0706d54b4b6592569012d2f641a9
Brian Clapper <bmc@clapper.org>
2011-03-14 22:52:49 -0400

    Fixed setup problem

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ info = load_info()
 
 NAME = 'paragrep'
 DOWNLOAD_URL = ('http://pypi.python.org/packages/source/p/%s/%s-%s.tar.gz' %
-                (NAME, NAME, module.version))
+                (NAME, NAME, info['__version__']))
 
 # Now the setup stuff.
 
